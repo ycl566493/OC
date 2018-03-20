b7124e3a38dc07cc2143245c9861a330e6d2e022
xuanyanhua <2322787013@qq.com>
2018-03-20 11:25:17 +0800

    ycl

---
+++ Checkmall/Checkmall/SSSSBBBBB.h
@@ -0,0 +1,13 @@
+//
+//  SSSSBBBBB.h
+//  Checkmall
+//
+//  Created by 杨成龙MAC on 2018/3/20.
+//  Copyright © 2018年 CKJY. All rights reserved.
+//sfasdf
+
+#import "BaseTableViewController.h"
+
+@interface SSSSBBBBB : BaseTableViewController
+
+@end
